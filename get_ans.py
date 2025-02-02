864223d067a38f72fdefb1506b51d91f603b48d8
XXRKSJLYC <XXRKSJLYC@88.com>
2025-02-02 17:23:35 +0800

    get_ans.py

--- get_ans.py
+++ get_ans.py
@@ -4,15 +4,15 @@ from openai import OpenAI
 # 初始化AI问答环境
 
 client = OpenAI(
-    api_key = "d193273f-5fd9-4ad7-8bdd-c23397145fdc",
-    base_url = "https://ark.cn-beijing.volces.com/api/v3",
+    api_key = "YOUR_API_KEY",
+    base_url = "YOUR_BASE_URL",
 )
 # 模型名称
-model_name="ep-20250201132243-cswlp"
+model_name="YOUR_MODEL_NAME"
 # 初始系统提示
 system_prompt="你是一个友好的助手，请用简短的语言回答问题，每次回复不要超过50个字"
 # 回答结尾提示信息
-end_prompt_information="——AI自动回复，输入stop即可停止"
+end_prompt_information="——AI自动回复"
 
 # 获取回答
 def answer_message(user_id,message):
